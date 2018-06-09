7889f5e2ca3b3f233ff86edecbf573175a471048
Akshar Raaj <akshar@agiliq.com>
2018-06-10 00:48:23 +0530

    First commit. Has apps polls and books

---
+++ books/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    isbn = models.CharField(max_length=100)
+
+    def __unicode__(self):
+        return self.title
