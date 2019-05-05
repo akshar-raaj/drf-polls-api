0b448655773de27f7a452a8bcfe8329e4120b39c
Akshar Raaj <akshar@agiliq.com>
2019-05-05 15:04:05 +0530

    Added ability to create associated choices for a question along with the question

--- polls/serializers.py
+++ polls/serializers.py
@@ -33,7 +33,16 @@ class QuestionListPageSerializer(serializers.ModelSerializer):
 
 
 class QuestionDetailPageSerializer(QuestionListPageSerializer):
-    choices = ChoiceSerializer(read_only=True, many=True)
+    choice_set = ChoiceSerializer(many=True)
+
+    def create(self, validated_data):
+        choice_validated_data = validated_data.pop('choice_set')
+        question = Question.objects.create(**validated_data)
+        choice_set_serializer = self.fields['choice_set']
+        for each in choice_validated_data:
+            each['question'] = question
+        choices = choice_set_serializer.create(choice_validated_data)
+        return question
 
 
 class QuestionResultPageSerializer(QuestionListPageSerializer):
