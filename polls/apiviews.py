94af91440ce30b0f796187cb70e424fb6d224aa9
Akshar Raaj <akshar@agiliq.com>
2019-05-06 00:39:15 +0530

    Used APIView instead of @api_view

--- polls/apiviews.py
+++ polls/apiviews.py
@@ -2,19 +2,21 @@ from django.shortcuts import get_object_or_404
 
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.views import APIView
 from rest_framework import status
 
 from .models import Question, Choice
 from .serializers import QuestionListPageSerializer, QuestionDetailPageSerializer, QuestionChoiceSerializer, VoteSerializer, QuestionResultPageSerializer, ChoiceSerializer
 
 
-@api_view(['GET', 'POST'])
-def questions_view(request):
-    if request.method == 'GET':
+class QuestionsView(APIView):
+
+    def get(self, request, *args, **kwargs):
         questions = Question.objects.all()
         serializer = QuestionListPageSerializer(questions, many=True)
         return Response(serializer.data)
-    elif request.method == 'POST':
+
+    def post(self, request, *args, **kwargs):
         serializer = QuestionDetailPageSerializer(data=request.data)
         if serializer.is_valid():
             question = serializer.save()
@@ -23,44 +25,52 @@ def questions_view(request):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-@api_view(['GET', 'PATCH', 'DELETE'])
-def question_detail_view(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    if request.method == 'GET':
+class QuestionDetailView(APIView):
+
+    def get(self, request, *args, **kwargs):
+        question = get_object_or_404(Question, pk=kwargs['question_id'])
         serializer = QuestionDetailPageSerializer(question)
         return Response(serializer.data)
-    elif request.method == 'PATCH':
+
+    def patch(self, request, *args, **kwargs):
+        question = get_object_or_404(Question, pk=kwargs['question_id'])
         serializer = QuestionDetailPageSerializer(question, data=request.data, partial=True)
         if serializer.is_valid():
             question = serializer.save()
             return Response(QuestionDetailPageSerializer(question).data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    elif request.method == 'DELETE':
+
+    def delete(self, request, *args, **kwargs):
+        question = get_object_or_404(Question, pk=kwargs['question_id'])
         question.delete()
         return Response("Question deleted", status=status.HTTP_204_NO_CONTENT)
 
 
-@api_view(['GET', 'POST'])
-def question_choices_view(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    if request.method == 'GET':
+class QuestionChoicesView(APIView):
+    
+    def get(self, request, *args, **kwargs):
+        question = get_object_or_404(Question, pk=kwargs['question_id'])
         choices = question.choice_set.all()
         serializer = QuestionChoiceSerializer(choices, many=True)
         return Response(serializer.data)
-    else:
+
+    def post(self, request, *args, **kwargs):
+        question = get_object_or_404(Question, pk=kwargs['question_id'])
         serializer = QuestionChoiceSerializer(data=request.data)
         if serializer.is_valid():
             choice = serializer.save(question=question)
             return Response("Choice created with id %s" % (choice.id), status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-@api_view(['GET', 'POST', 'DELETE'])
-def choices_view(request):
-    if request.method == 'GET':
+
+class ChoicesView(APIView):
+
+    def get(self, request, *args, **kwargs):
         choices = Choice.objects.all()
         serializer = ChoiceSerializer(choices, many=True)
         return Response(serializer.data)
-    elif request.method == 'POST':
+
+    def post(self, request, *args, **kwargs):
         serializer = ChoiceSerializer(data=request.data)
         if serializer.is_valid():
             choice = serializer.save()
@@ -68,20 +78,22 @@ def choices_view(request):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-@api_view(['PATCH'])
-def vote_view(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    serializer = VoteSerializer(data=request.data)
-    if serializer.is_valid():
-        choice = get_object_or_404(Choice, pk=serializer.validated_data['choice_id'], question=question)
-        choice.votes += 1
-        choice.save()
-        return Response("Voted")
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class VoteView(APIView):
+
+    def patch(self, request, *args, **kwargs):
+        question = get_object_or_404(Question, pk=kwargs['question_id'])
+        serializer = VoteSerializer(data=request.data)
+        if serializer.is_valid():
+            choice = get_object_or_404(Choice, pk=serializer.validated_data['choice_id'], question=question)
+            choice.votes += 1
+            choice.save()
+            return Response("Voted")
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-@api_view(['GET'])
-def question_result_view(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    serializer = QuestionResultPageSerializer(question)
-    return Response(serializer.data)
+class QuestionResultView(APIView):
+
+    def get(self, request, *args, **kwargs):
+        question = get_object_or_404(Question, pk=kwargs['question_id'])
+        serializer = QuestionResultPageSerializer(question)
+        return Response(serializer.data)
--- polls/urls.py
+++ polls/urls.py
@@ -5,11 +5,11 @@ from . import apiviews
 
 app_name = 'polls'
 urlpatterns = [
-    path('questions/', apiviews.questions_view, name='questions_view'),
-    path('questions/<int:question_id>/', apiviews.question_detail_view, name='question_detail_view'),
-    path('questions/<int:question_id>/choices/', apiviews.question_choices_view, name='choices_view'),
-    path('questions/<int:question_id>/vote/', apiviews.vote_view, name='vote_view'),
-    path('questions/<int:question_id>/result/', apiviews.question_result_view, name='question_result_view'),
+    path('questions/', apiviews.QuestionsView.as_view(), name='questions_view'),
+    path('questions/<int:question_id>/', apiviews.QuestionDetailView.as_view(), name='question_detail_view'),
+    path('questions/<int:question_id>/choices/', apiviews.QuestionChoicesView.as_view(), name='choices_view'),
+    path('questions/<int:question_id>/vote/', apiviews.VoteView.as_view(), name='vote_view'),
+    path('questions/<int:question_id>/result/', apiviews.QuestionResultView.as_view(), name='question_result_view'),
 
-    path('choices/', apiviews.choices_view, name='choices_view'),
+    path('choices/', apiviews.ChoicesView.as_view(), name='choices_view'),
 ]
