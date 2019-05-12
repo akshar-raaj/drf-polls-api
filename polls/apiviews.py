4a5d64b32f93de013980a459c9556256dfa93834
Akshar Raaj <akshar@agiliq.com>
2019-05-12 10:44:58 +0530

    Able to do list, create, retrieve, partial_update and delete on /polls/yet-another-questions/

--- polls/apiviews.py
+++ polls/apiviews.py
@@ -5,7 +5,7 @@ from rest_framework.views import APIView
 from rest_framework.generics import ListCreateAPIView, RetrieveAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.pagination import PageNumberPagination
 from rest_framework import status
-from rest_framework.viewsets import ViewSet, ReadOnlyModelViewSet
+from rest_framework.viewsets import ViewSet, ReadOnlyModelViewSet, ModelViewSet
 
 from .models import Question, Choice
 from .serializers import QuestionListPageSerializer, QuestionDetailPageSerializer, QuestionChoiceSerializer, VoteSerializer, QuestionResultPageSerializer, ChoiceSerializer
@@ -72,7 +72,7 @@ class QuestionResultView(RetrieveAPIView):
     lookup_url_kwarg = 'question_id'
 
 
-class YetAnotherQuestionsViewSet(ReadOnlyModelViewSet):
+class YetAnotherQuestionsViewSet(ModelViewSet):
     queryset = Question.objects.all()
     serializer_class = QuestionDetailPageSerializer
     lookup_url_kwarg = 'question_id'
