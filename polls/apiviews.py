aed8588a491ff23fee82e93854244c1717175dd5
Akshar Raaj <akshar@agiliq.com>
2019-05-12 19:18:31 +0530

    Added documentation following apidocjs format. Documentation can be created by executing 'apidoc -i polls/ -o doc/'

--- polls/apiviews.py
+++ polls/apiviews.py
@@ -16,6 +16,77 @@ class ChoicesView(ListCreateAPIView):
 
 
 class QuestionsViewSet(ModelViewSet):
+    """
+    @api {get} /questions/ Questions list
+    @apiName Questions list
+    @apiDescription This returns a list of questions.
+    @apiGroup Questions
+    @apiVersion 1.0.0
+
+    @apiSuccess {Integer} id Unique identifier
+    @apiSuccess {String} pub_date Published date
+    @apiSuccess {String} question_text Question text
+    @apiSuccess {String} was_published_recently Was published recently?
+
+    @apiSuccessExample Response
+        HTTP/1.1 200 OK
+        [
+            {
+                "id": 1,
+                "was_published_recently": false,
+                "question_text": "Who is the most likable character in GOT?"
+                "pub_date": "2019-05-09T00:00:00Z",
+            },
+            {
+                "id": 46,
+                "was_published_recently": false,
+                "question_text": "Who is your favorite fictional character?",
+                "pub_date": "2019-05-10T00:00:00Z"
+            }
+        ]
+    """
+    """
+    @api {post} /questions/ Create a question
+    @apiName Question create
+    @apiGroup Questions
+    @apiVersion 1.0.0
+
+    @apiParam (Payload) {String} pub_date Published date
+    @apiParam (Payload) {String} question_text Question text
+    @apiParam (Payload) {Dictionary[]} choice_set List of choices
+
+    @apiParamExample {json} Payload-example
+        {
+            "pub_date": "2019-05-05T00:00",
+            "question_text": "Is Samsung more reliable than iPhone?",
+            "choice_set": [
+                {
+                    "choice_text": "No"
+                }
+            ]
+        }
+
+    @apiSuccess {Integer} id Unique identifier
+    @apiSuccess {Dictionary[]} choice_set List of choices
+    @apiSuccess {String} pub_date Published date
+    @apiSuccess {String} question_text Question text
+    @apiSuccess {String} was_published_recently Was published recently?
+
+    @apiSuccessExample Response
+        HTTP/1.1 201 Created
+        {
+            "id": 50,
+            "was_published_recently": false,
+            "choice_set": [
+                {
+                    "id": 50,
+                    "choice_text": "No"
+                }
+            ],
+            "question_text": "Is Samsung more reliable than iPhone?",
+            "pub_date": "2019-05-05T00:00:00Z"
+        }
+    """
     queryset = Question.objects.all()
     lookup_url_kwarg = 'question_id'
 
