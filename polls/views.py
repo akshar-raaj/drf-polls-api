95039d954e9fc36cc3889d05368f4e538d9f9a4d
Akshar Raaj <akshar@agiliq.com>
2019-07-07 20:12:11 +0530

    Added a custom filter

--- polls/views.py
+++ polls/views.py
@@ -5,6 +5,13 @@ from .models import Question
 from .serializers import QuestionSerializer
 
 
+class SearchFilterWithSpaces(filters.SearchFilter):
+    def get_search_terms(self, request):
+        params = request.query_params.get(self.search_param, '')
+        terms =  params.split(",")
+        return [term.strip() for term in terms]
+
+
 class QuestionsAPIView(generics.ListCreateAPIView):
     search_fields = ['^question_text', 'author']
     filter_backends = (filters.SearchFilter,)
