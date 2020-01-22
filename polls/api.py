78b20c6719689d69fcd7627e196047f8ff18bd00
Akshar Raaj <akshar@agiliq.com>
2020-01-22 18:50:24 +0530

    Integrating with Tastypie. Added few basic modelresources and tied them with urlpattern

---
+++ polls/api.py
@@ -0,0 +1,28 @@
+from .models import Question, Choice
+
+from tastypie.resources import ModelResource
+from tastypie import fields
+from tastypie.authorization import Authorization
+
+
+class QuestionResource(ModelResource):
+
+    class Meta:
+        # queryset = Question.objects.all()
+        object_class = Question
+        resource_name = 'questions'
+        authorization = Authorization()
+        # excludes = ('id',)
+
+
+class ChoiceResource(ModelResource):
+
+    question = fields.ForeignKey(QuestionResource, 'question', full=True)
+
+    class Meta:
+        queryset = Choice.objects.all()
+        resource_name = 'choices'
+
+class PersonResource(ModelResource):
+
+    name = fields.CharField()
--- polls/urls.py
+++ polls/urls.py
@@ -1,8 +1,10 @@
-from django.urls import path
+from django.urls import path, include
 
 from rest_framework.routers import SimpleRouter
 
 from . import apiviews
+from . import api
+from tastypie.api import Api
 
 app_name = 'polls'
 urlpatterns = [
@@ -15,3 +17,18 @@ router = SimpleRouter()
 router.register('questions', apiviews.QuestionsViewSet, basename='question')
 
 urlpatterns += router.urls
+
+question_resource = api.QuestionResource()
+choice_resource = api.ChoiceResource()
+
+v1_api = Api(api_name='v1')
+v1_api.register(api.QuestionResource())
+v1_api.register(api.ChoiceResource())
+
+tastypie_patterns = [
+    path(r'tastypie/', include(question_resource.urls)),
+    path(r'tastypie/', include(choice_resource.urls)),
+    path(r'api/', include(v1_api.urls)),
+]
+
+urlpatterns += tastypie_patterns
