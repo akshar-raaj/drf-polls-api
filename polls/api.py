324472d47d273861304a2806a0915b5cd82ec167
Akshar Raaj <akshar@agiliq.com>
2020-01-23 13:51:45 +0530

    Modified authorization to allow POST on list endpoint

--- polls/api.py
+++ polls/api.py
@@ -16,34 +16,15 @@ class ChoiceResource(ModelResource):
 
 class QuestionResource(ModelResource):
 
-    question_text_duplicate = fields.CharField(attribute='question_text')
-    question_text_lower = fields.CharField(attribute='question_text_lower')
-    choices = fields.ManyToManyField(ChoiceResource, attribute='choice_set', full=True)
+    question_text_duplicate = fields.CharField(attribute='question_text', readonly=True)
 
     class Meta:
-        # queryset = Question.objects.all()
+        queryset = Question.objects.all()
         object_class = Question
         resource_name = 'questions'
+        list_allowed_methods = ['get', 'post']
         authorization = Authorization()
-        # excludes = ('id',)
-
-    def dehydrate_question_text(self, bundle):
-        return bundle.data['question_text'].upper()
-
-    def build_filters(self, filters=None):
-        filters = super(QuestionResource, self).build_filters(filters)
-        filters['id__gt'] = 72
-        return filters
-
-    def get_object_list(self, request):
-        return Question.objects.filter(id__gt=71)
 
     def dehydrate(self, bundle):
         bundle.data['len_question_text'] = len(bundle.data['question_text'])
         return bundle
-
-
-
-class PersonResource(ModelResource):
-
-    name = fields.CharField()
